72b2c41c2a7b791add6f55a35f606e625db2a352
mrc0302 <167983901+mrc0302@users.noreply.github.com>
2025-02-07 09:17:34 -0300

    Update formSquill.py

--- formSquill.py
+++ formSquill.py
@@ -213,11 +213,11 @@ def main():
             st.session_state.dialog_open = False
 
 
-    # def load_css(file_path):
-    #     with open(file_path) as f:
-    #         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    def load_css(file_path):
+         with open(file_path) as f:
+             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-    #load_css("static/styles.css")
+    load_css("static/styles.css")
 
     @st.dialog("Aviso") 
     def show_confirmation_export():      
