a57b28db231dadfcdbd7286a42fe3a255ef25e8c
mrc0302 <167983901+mrc0302@users.noreply.github.com>
2025-02-12 09:19:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import formSquill as fs
 import langchainSearch as ls
 import conversorRTF_HTML as crtf
-import ragTeste as rg
+#import ragTeste as rg
 
 
 
@@ -92,7 +92,7 @@ if selected == "Formulário":
 
 elif selected == "Chatbot Jurídico":
     
-   rg 
+   st.write("chabot") 
     
 elif selected == "Conversor de banco de dados":
     
