b44381d2663f95bd5da89383f92aa62170a8f5ec
mrc0302 <167983901+mrc0302@users.noreply.github.com>
2025-02-12 09:32:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,6 @@ import langchainSearch as ls
 import conversorRTF_HTML as crtf
 import ragTeste as rg
 
-
-
-
 st.set_page_config(page_title="Gerenciador de Modelos Judiciais", layout="wide")
 
 # st.markdown("""
@@ -43,66 +40,15 @@ st.set_page_config(page_title="Gerenciador de Modelos Judiciais", layout="wide")
 with st.sidebar:
 
     
-        pg = st.navigation([st.Page("langchainSearch.py", title= "Pesquisa Avançada por IA", icon="🎯"), 
+  pg = st.navigation([st.Page("langchainSearch.py", title= "Pesquisa Avançada por IA", icon="🎯"), 
                               st.Page("ragTeste.py",title= "Chatbot RAG", icon="🤖"), 
                               st.Page("formSquill.py",title= "Tela de Formulário", icon="📝") , 
-                                 st.Page("conversorRTF_HTML.py",title= "Conversor de banco de dados RTF para HTML", icon="📄")])
+                              st.Page("conversorRTF_HTML.py",title= "Conversor de banco de dados RTF para HTML", icon="📄")])
 pg.run()   
-    # selected = option_menu(
-    #     menu_title="Menu Principal",  # required
-    #     options=["Formulário", "Pesquisa Avançada", "Chatbot Jurídico"],  # required
-    # )
-
-    # if selected == "Formulário":
-    #     fs.main()
-    # if selected == "Pesquisa Avançada":
-    #     ls.main()
-    # if selected == "Chatbot Jurídico":
-    #      rg.main() 
-
-
-
-
-selected = option_menu(
-     menu_title=None,
-     options=["Formulário", "Chatbot Jurídico", "Pesquisa Avançada","Conversor de banco de dados"], # Você pode alterar estas opções
-     icons=["house", "file-text", "eye"],         # E estes ícones
-     default_index=0,                             # Página inicial (0 = primeiro item)
-     orientation="horizontal",                     # Navegação horizontal
-     styles={
-         "nav-link": {
-             "font-size": "14px",
-             "text-align": "center",
-             "margin": "0px",
-             "--hover-color": "#eee",
-         },
-         "nav-link-selected": {"background-color": "#0083B8"},
-     }
- )
-
-# # Lógica das páginas
-if selected == "Formulário":
-    
-   fs
-
-elif selected == "Chatbot Jurídico":
-    
-   st.write("chabot") 
-    
-elif selected == "Conversor de banco de dados":
-    
-   crtf
-
-elif selected == "Pesquisa Avançada":
    
-    ls
-    
 
 
 
-
-#
-        
     
    
 
