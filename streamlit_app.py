dd6e8654bda900edc0c1391a7da8e516c5a79849
mrc0302 <167983901+mrc0302@users.noreply.github.com>
2025-02-18 20:16:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,53 +5,70 @@ import langchainSearch as ls
 import conversorRTF_HTML as crtf
 import ragTeste as rg
 
-st.set_page_config(page_title="Gerenciador de Modelos Judiciais", layout="wide")
 
-# st.markdown("""
-#     <style>
-#         .main {
-#             overflow: hidden;
-#         }
-#         .block-container {
-#             padding-top: 1rem;
-#             padding-bottom: 0rem;
-#             height: 100vh;
-#         }
-#         footer {
-#             display: none;
-#         }
-#         #MainMenu {
-#             visibility: hidden;
-#         }
-#         header {
-#             visibility: hidden;
-#         }                  
+# Configuração da página deve ser o primeiro comando Streamlit
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊", 
+    layout="wide",  
+   # initial_sidebar_state="expanded"
+)
+
+st.markdown("""
+    <style>
+        .main {
+            overflow: hidden;
+        }
+        .block-container {
+            padding-top: 1rem;
+            padding-bottom: 0rem;
+            height: 100vh;
+        }
+        footer {
+            display: none;
+        }
+        #MainMenu {
+            visibility: hidden;
+        }
+        header {
+            visibility: hidden;
+        }                  
         
-#     </style>
-# """, unsafe_allow_html=True)
+    </style>
+""", unsafe_allow_html=True)
 
 
-# st.markdown(f"""<header tabindex="-1" class="stAppHeader st-emotion-cache-12fmjuu e10jh26i0"><center><h2>💬 Chat Assistente Jurídico</h2></center></header> 
-#     """,
-#     unsafe_allow_html=True
-# )
+selected = option_menu(
+    menu_title=None,
+    options=["Formulário", "Chatbot Jurídico", "Pesquisa Avançada","Conversor de banco de dados"], # Você pode alterar estas opções
+    icons=["house", "file-text", "eye"],         # E estes ícones
+    default_index=0,                             # Página inicial (0 = primeiro item)
+    orientation="horizontal",                     # Navegação horizontal
+    styles={
+        "nav-link": {
+            "font-size": "14px",
+            "text-align": "center",
+            "margin": "0px",
+            "--hover-color": "#eee",
+        },
+        "nav-link-selected": {"background-color": "#0083B8"},
+    }
+)
 
-
-with st.sidebar:
-
+# Lógica das páginas
+if selected == "Formulário":
     
-  pg = st.navigation([st.Page("langchainSearch.py", title= "Pesquisa Avançada por IA", icon="🎯"), 
-                              st.Page("ragTeste.py",title= "Chatbot RAG", icon="🤖"), 
-                              st.Page("formSquill.py",title= "Tela de Formulário", icon="📝") , 
-                              st.Page("conversorRTF_HTML.py",title= "Conversor de banco de dados RTF para HTML", icon="📄")])
-pg.run()   
-   
+    fs.main()
 
-
-
+elif selected == "Chatbot Jurídico":
     
+    rg.main() 
+    
+elif selected == "Conversor de banco de dados":
+    
+    crtf.main()
+
+elif selected == "Pesquisa Avançada":
    
-
-#st.caption("Desenvolvido por: [Langchain](https://www.langchain.com.br/)")
-
-#stAppHeader st-emotion-cache-12fmjuu e10jh26i0
+    ls.main()
+  
